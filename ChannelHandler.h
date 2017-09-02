39ca4314f9c9b116e7fbcf8a7ba36a78a5af0a77
stedaho <github@stedaho.de>
2017-09-02 22:53:58 +0200

    * Added gitignore file to ignore hex files created by Arduino IDE * Added real name to license file * First checkin of the current state of the controller software

---
+++ ChannelHandler.h
@@ -0,0 +1,69 @@
+/* Maximum number of switching times per day */
+const int MAX_VALUES = 15;
+
+class ChannelHandler {
+    unsigned int (&valueTable)[MAX_VALUES][2];
+    int numberValues = MAX_VALUES;
+    int currentIndex = 0;
+
+  public:
+    /* Constructor gets a reference to the time table */
+    ChannelHandler(unsigned int (&valueTableParam)[MAX_VALUES][2]) : valueTable(valueTableParam) {
+      // TODO Validate times (must be ascending and between 0 and 24h) and values (must be between 0 and 1000)
+      /* Determine the number of switching times per day */
+      for (int i = 1; i < MAX_VALUES; i++) {
+        if (valueTable[i][0] == 0) {
+          numberValues = i;
+          break;
+        }
+      }
+    }
+
+    /* Calculates the light percentage value at the given time */
+    float GetPercentageAtTime(long time) {
+      int currentIdx = GetCurrentIndex(time);
+      int nextIdx = GetNextIndex();
+
+      float result = CalculateValue(valueTable[currentIdx][0], valueTable[currentIdx][1], valueTable[nextIdx][0], valueTable[nextIdx][1], time);
+      return constrain(result, 0, 100);
+    }
+
+  private:
+    /* Returns the position in the timetable we are at the moment */
+    int GetCurrentIndex(long time) {
+      FindCurrentIndex(time);
+      return currentIndex;
+    }
+
+    /* Finds the position in the timetable we are at the moment */
+    void FindCurrentIndex(long time) {
+      while (!(
+               ((long)valueTable[currentIndex][0] * 10 <= time && (long)valueTable[GetNextIndex()][0] * 10 > time) || /* Normally we are between two time value */
+               (currentIndex == (numberValues - 1) && (long)valueTable[currentIndex][0] * 10 <= time) || /* End of day, after the last time value */
+               (currentIndex == (numberValues - 1) && (long)valueTable[GetNextIndex()][0] * 10 > time) )) { /* Start of day, before the first time value */
+        currentIndex = GetNextIndex();
+      }
+    }
+
+    /* Determines the next index in the timetable */
+    int GetNextIndex() {
+      if (currentIndex < (numberValues - 1)) {
+        return (currentIndex + 1);
+      }
+      return 0;
+    }
+
+    /* Determines the previous index in the timetable */
+    int GetPrevIndex() {
+      if (currentIndex > 0) {
+        return (currentIndex - 1);
+      }
+      return (numberValues - 1);
+    }
+
+    /* Calculates the value at the given time using a linear interpolation between the value at the previous and the next timestamp in the timetable */
+    float CalculateValue(long startTime, float startValue, long endTime, float endValue, long time) {
+      float timeDiff = ((endTime - startTime) + 8640) % 8640;
+      return ((((time - (startTime * 10)) / (timeDiff * 10)) * (endValue - startValue)) + startValue) / 10;
+    }
+};
---
+++ main.ino
@@ -0,0 +1,203 @@
+#include "ChannelHandler.h"
+#include "Wire.h"
+#include "uRTCLib.h"
+
+/* Pin assignment */
+const int pinWhite = 5;
+const int pinRed = 9;
+const int pinBlue = 6;
+const int pinButton1 = 2;
+
+const long MINUTE = 60;
+const long HOUR = 60 * MINUTE;
+const float maintenancePercentageValue = 90;
+
+/* Time-value pairs for white channel */
+unsigned int valueTableWhite[MAX_VALUES][2] {
+  {(7 * HOUR + 50 * MINUTE) / 10, 0},
+  {(8 * HOUR + 10 * MINUTE) / 10, 900},
+  {(13 * HOUR + 30 * MINUTE) / 10, 900},
+  {(13 * HOUR + 45 * MINUTE) / 10, 100},
+  {(16 * HOUR + 30 * MINUTE) / 10, 100},
+  {(16 * HOUR + 45 * MINUTE) / 10, 900},
+  {(21 * HOUR + 50 * MINUTE) / 10, 900},
+  {(22 * HOUR + 10 * MINUTE) / 10, 0}
+};
+
+/* Time-value pairs for red channel */
+unsigned int valueTableRed[MAX_VALUES][2] {
+  {(7 * HOUR + 30 * MINUTE) / 10, 0},
+  {(7 * HOUR + 50 * MINUTE) / 10, 900},
+  {(13 * HOUR + 30 * MINUTE) / 10, 900},
+  {(13 * HOUR + 45 * MINUTE) / 10, 100},
+  {(16 * HOUR + 30 * MINUTE) / 10, 100},
+  {(16 * HOUR + 45 * MINUTE) / 10, 900},
+  {(22 * HOUR + 10 * MINUTE) / 10, 900},
+  {(22 * HOUR + 30 * MINUTE) / 10, 0}
+};
+
+/* Time-value pairs for blue channel */
+unsigned int valueTableBlue[MAX_VALUES][2] {
+  {(0 * HOUR + 00 * MINUTE) / 10, 0},
+  {(8 * HOUR + 10 * MINUTE) / 10, 0},
+  {(8 * HOUR + 30 * MINUTE) / 10, 900},
+  {(13 * HOUR + 30 * MINUTE) / 10, 900},
+  {(13 * HOUR + 45 * MINUTE) / 10, 100},
+  {(16 * HOUR + 30 * MINUTE) / 10, 100},
+  {(16 * HOUR + 45 * MINUTE) / 10, 900},
+  {(21 * HOUR + 30 * MINUTE) / 10, 900},
+  {(21 * HOUR + 50 * MINUTE) / 10, 0},
+  {(22 * HOUR + 25 * MINUTE) / 10, 0},
+  {(22 * HOUR + 30 * MINUTE) / 10, 100}
+};
+
+/* Instances of channel handlers */
+ChannelHandler whiteChannel(valueTableWhite);
+ChannelHandler redChannel(valueTableRed);
+ChannelHandler blueChannel(valueTableBlue);
+/* Instance of the realtime clock */
+uRTCLib rtc;
+
+bool blinkerState = true;
+long currentTime = 0;
+volatile bool simulationRunning = false;
+volatile bool isInMaintenance = false;
+volatile bool maintenanceStateChangeHandled = true;
+unsigned long previousMillis = 0;
+
+
+/* Initial configuration */
+void setup() {
+  /* Configuration of inputs and outputs */
+  pinMode(pinWhite, OUTPUT);
+  pinMode(pinRed, OUTPUT);
+  pinMode(pinBlue, OUTPUT);
+  pinMode(pinButton1, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+  /* Set initial values */
+  analogWrite(pinWhite, 0);
+  analogWrite(pinRed, 0);
+  analogWrite(pinBlue, 0);
+  /* Configure serial interface */
+  delay(1000);
+  Serial.begin(57600);
+  delay(500);
+  /* Eventually set time */
+  // rtc.set(0, 0, 22, 6, 25, 8, 17); // RTCLib::set(byte second, byte minute, byte hour, byte dayOfWeek, byte dayOfMonth, byte month, byte year)
+  /* Configure button interrupt */
+  attachInterrupt(digitalPinToInterrupt(pinButton1), button1Pressed, RISING);
+  Serial.println("Aquariensteuerung gestartet!");
+}
+
+
+/* The loop */
+void loop() {
+  unsigned long currentMillis = millis();
+
+  /* Check if the loop has to run and determine current time - either simulated or from rtc */
+  if (simulationRunning) {
+    if (currentMillis - previousMillis < 5) {
+      return;
+    }
+    currentTime += MINUTE / 4;
+    currentTime %= (24 * HOUR);
+  } else {
+    if (currentMillis - previousMillis < 1000) {
+      return;
+    }
+    rtc.refresh();
+    currentTime = rtc.hour() * HOUR + rtc.minute() * MINUTE + rtc.second();
+  }
+  /* Save the time of the last run */
+  previousMillis = currentMillis;
+
+  /* Blink each second */
+  digitalWrite(LED_BUILTIN, blinkerState);
+  blinkerState = !blinkerState;
+
+  /* Get LED values for the given time */
+  float percentageValueWhite = whiteChannel.GetPercentageAtTime(currentTime);
+  float percentageValueRed = redChannel.GetPercentageAtTime(currentTime);
+  float percentageValueBlue = blueChannel.GetPercentageAtTime(currentTime);
+
+  /* Override values if we are in maintenance mode */
+  if (isInMaintenance) {
+    digitalWrite(LED_BUILTIN, true);
+    percentageValueWhite = maintenancePercentageValue;
+    percentageValueRed = maintenancePercentageValue;
+    percentageValueBlue = maintenancePercentageValue;
+    Serial.println("Maintenance mode active");
+  }
+  maintenanceStateChangeHandled = true;
+
+  /* Convert percentage values to raw PWM values */
+  int rawValueWhite = GetRawFromPercentage(GammaCorrect(percentageValueWhite));
+  int rawValueRed = GetRawFromPercentage(GammaCorrect(percentageValueRed));
+  int rawValueBlue = GetRawFromPercentage(GammaCorrect(percentageValueBlue));
+
+  /* Set PWM values to the LED outputs */
+  analogWrite(pinWhite, rawValueWhite);
+  analogWrite(pinRed, rawValueRed);
+  analogWrite(pinBlue, rawValueBlue);
+
+  PrintSerialOutput(currentTime, percentageValueWhite, percentageValueRed, percentageValueBlue);
+}
+
+
+/* Handles the interrupt for button 1 pressed */
+void button1Pressed() {
+  if (maintenanceStateChangeHandled) {
+    isInMaintenance = !isInMaintenance;
+    maintenanceStateChangeHandled = false;
+  }
+}
+
+
+/* Handles the incoming serial data event */
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    if (inChar == 's') {
+      simulationRunning = true;
+    } else if (inChar == 'r') {
+      simulationRunning = false;
+    }
+  }
+}
+
+
+/* Performs a simple gamma correction of the given percentage value */
+float GammaCorrect(float percentageValue) {
+  return percentageValue * percentageValue / 100;
+  // return pow(percentageValue, 2.7) / 2512;
+}
+
+
+/* Converts the percentage value into a raw PWM value */
+int GetRawFromPercentage(float percentageValue) {
+  int rawValue = percentageValue * 2.55 + 0.5;
+  /* Set the raw value to 1 if it would be to small to have a guaranteed output if the percentage value is greater zero */
+  if (percentageValue > 0 && rawValue == 0) {
+    rawValue = 1;
+  }
+  return rawValue;
+}
+
+
+/* Prints the current time and values */
+void PrintSerialOutput(long timer, float vWhite, float vRed, float vBlue) {
+  char output[100];
+
+  sprintf(output, "[%02d:%02d:%02d] White: %02d.%01d%% Red: %02d.%01d%% Blue: %02d.%01d%%",
+          (int)(timer / HOUR),
+          (int)((timer % HOUR) / MINUTE),
+          (int)(timer % MINUTE),
+          (int)vWhite,
+          (int)(vWhite * 10) % 10,
+          (int)vRed,
+          (int)(vRed * 10) % 10,
+          (int)vBlue,
+          (int)(vBlue * 10) % 10);
+  Serial.println(output);
+}
+
