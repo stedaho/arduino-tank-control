60a30e3fd9dcb2e4911df3b951c3d695210b17da
stedaho <github@stedaho.de>
2017-10-08 19:43:58 +0200

    * Time table adjusted (lights go off at 10 pm, less overlapping between daylight and moon)

--- TimeTables.h
+++ TimeTables.h
@@ -6,39 +6,39 @@ const float maintenancePercentageValue = 90;
 
 /* Time-value pairs for white channel */
 unsigned int valueTableWhite[MAX_VALUES][2] {
-  {(7 * HOUR + 50 * MINUTE) / 10, 0},
-  {(8 * HOUR + 10 * MINUTE) / 10, 900},
+  {( 7 * HOUR + 50 * MINUTE) / 10, 0},
+  {( 8 * HOUR + 10 * MINUTE) / 10, 900},
   {(13 * HOUR + 30 * MINUTE) / 10, 900},
   {(13 * HOUR + 45 * MINUTE) / 10, 50},
   {(16 * HOUR + 30 * MINUTE) / 10, 50},
   {(16 * HOUR + 45 * MINUTE) / 10, 900},
-  {(21 * HOUR + 50 * MINUTE) / 10, 900},
-  {(22 * HOUR + 10 * MINUTE) / 10, 0}
+  {(21 * HOUR + 20 * MINUTE) / 10, 900},
+  {(21 * HOUR + 40 * MINUTE) / 10, 0}
 };
 
 /* Time-value pairs for red channel */
 unsigned int valueTableRed[MAX_VALUES][2] {
-  {(7 * HOUR + 30 * MINUTE) / 10, 0},
-  {(7 * HOUR + 50 * MINUTE) / 10, 900},
+  {( 7 * HOUR + 30 * MINUTE) / 10, 0},
+  {( 7 * HOUR + 50 * MINUTE) / 10, 900},
   {(13 * HOUR + 30 * MINUTE) / 10, 900},
   {(13 * HOUR + 45 * MINUTE) / 10, 50},
   {(16 * HOUR + 30 * MINUTE) / 10, 50},
   {(16 * HOUR + 45 * MINUTE) / 10, 900},
-  {(22 * HOUR + 10 * MINUTE) / 10, 900},
-  {(22 * HOUR + 30 * MINUTE) / 10, 0}
+  {(21 * HOUR + 40 * MINUTE) / 10, 900},
+  {(22 * HOUR +  0 * MINUTE) / 10, 0}
 };
 
 /* Time-value pairs for blue channel */
 unsigned int valueTableBlue[MAX_VALUES][2] {
-  {(0 * HOUR + 00 * MINUTE) / 10, 0},
-  {(8 * HOUR + 10 * MINUTE) / 10, 0},
-  {(8 * HOUR + 30 * MINUTE) / 10, 900},
+  {( 0 * HOUR +  0 * MINUTE) / 10, 0},
+  {( 8 * HOUR + 10 * MINUTE) / 10, 0},
+  {( 8 * HOUR + 30 * MINUTE) / 10, 900},
   {(13 * HOUR + 30 * MINUTE) / 10, 900},
   {(13 * HOUR + 45 * MINUTE) / 10, 50},
   {(16 * HOUR + 30 * MINUTE) / 10, 50},
   {(16 * HOUR + 45 * MINUTE) / 10, 900},
-  {(21 * HOUR + 30 * MINUTE) / 10, 900},
-  {(21 * HOUR + 50 * MINUTE) / 10, 0},
-  {(22 * HOUR + 29 * MINUTE) / 10, 0},
-  {(22 * HOUR + 30 * MINUTE) / 10, 30}
+  {(21 * HOUR +  0 * MINUTE) / 10, 900},
+  {(21 * HOUR + 20 * MINUTE) / 10, 0},
+  {(21 * HOUR + 59 * MINUTE + 50) / 10, 0},
+  {(22 * HOUR +  0 * MINUTE) / 10, 30}
 };
